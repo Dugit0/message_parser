2c012748144ecbc7eca5a9fb02057c6e7510a831
Dmitry <dmitry.astrakhantsev@gmail.com>
2023-01-21 14:23:07 +0300

    Add message types

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 #!/bin/python
 
 from bs4 import BeautifulSoup
+import enum
 
+
+class MessageType(enum.Enum):
+    voice = 1
+    circle = 2
+    call = 3
+    sticker = 4
+    common = 5
+
+
+class Message:
+    def __init__(self, soup, author):
+        self.author = author
+    pass
+
+
+class Chat:
+    pass
 
 
 # def get_name()
