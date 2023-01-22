36580eb7f3a01b52b6d211f69ee899b99c190949
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-01-22 15:46:49 +0300

    Add func get_all_message_files

--- main.py
+++ main.py
@@ -23,27 +23,33 @@ class Chat:
     pass
 
 
-# def get_name()
+def get_all_message_files():
+    chats_path = os.path.join("data", "chats")
+    all_message_files_list = []
+    for chat_num in os.listdir(chats_path):
+        for file in os.listdir(os.path.join(chats_path, chat_num)):
+            if file[-5:] == ".html":
+                all_message_files_list.append(os.path.join(chats_path, chat_num, file))
+    print(all_message_files_list)
+
+# =============================================================================
+# data_path = os.path.join("data", "chats", "chat_008", "messages.html")
+# 
+# with open(data_path, encoding="utf-8") as data_file:
+#     soup = BeautifulSoup(data_file, "lxml")
+#     messages_list = soup.body.div.find("div", class_="page_body").div
+# 
+#     messages_list = messages_list.find_all(\
+#         lambda tag: tag.get("class") != None and "message" in tag.get("class"))
+#     for i in range(5):
+#         print(messages_list[i].prettify())
+#         print("----------")
+# =============================================================================
 
 
 
-
-
-
-
-
-
-data_path = os.path.join("data", "chats", "chat_008", "messages.html")
-
-with open(data_path, encoding="utf-8") as data_file:
-    soup = BeautifulSoup(data_file, "lxml")
-    messages_list = soup.body.div.find("div", class_="page_body").div.find_all(lambda tag: tag["id"][:7] == "message")
-    for i in range(5):
-        print(messages_list[i].prettify())
-        print("----------")
     # with open("out.html", "w", encoding="utf-8") as f_out:
     #     print(messages_list.prettify(), file=f_out)
-
 
 
 # print(soup.prettify())
