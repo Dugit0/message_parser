2295f1fe6ae540e07bb0e4de369a952b373849e7
Dugit0 <dmitry.astrakhantsev@gmail.com>
2023-04-16 16:02:59 +0300

    Add get_real_chat_names func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/home/dmitry/IT/Fun/message_parser/venv/bin/python
+import sys
 
 from bs4 import BeautifulSoup
 import enum
@@ -14,6 +15,9 @@ import datetime
 #     call = 3
 #     sticker = 4
 #     common = 5
+
+# class ChatType(enum.Enum):
+#     default = 0
 
 
 class Message:
@@ -52,21 +56,22 @@ class Common(Message):
     pass
 
 
+class Forwarded(Message):
+    pass
+
+
 class Chat:
     def __init__(self, file_chat_name, real_name):
         self.chat_name = real_name
         soups = get_message_soups(file_chat_name)
         self.messages = []
-        last_author = None
+        name = None
         for soup in soups:
             if not ("joined" in soup["class"]):
                 name_tag = soup.find("div", class_="body").find("div", class_="from_name")
                 name = name_tag.text.strip()
-                last_author = name
-                self.messages.append(Message(soup, name))
-            else:
-                assert last_author != None
-                self.messages.append(Message(soup, last_author))
+            assert not (name is None)
+            self.messages.append(Message(soup, name))
 
 
 def get_all_message_files():
@@ -96,27 +101,56 @@ def get_message_soups(chat):
     return message_soups
 
 
-def get_html_from_dir():
-    pass
+def get_real_chat_names():
+    list_chats_file_path = os.path.join("data", "lists", "chats.html")
+    with open(list_chats_file_path, encoding="utf-8") as list_chats_file:
+        soup = BeautifulSoup(list_chats_file, "lxml")
+    chats_lists = soup.body.find("div", class_="page_wrap").find("div", class_="page_body").find_all("div",
+                                                                                                     class_="entry_list")
+    all_chats = []
+    for chats_list in chats_lists:
+        all_chats.extend(chats_list.find_all(class_="entry"))
+    max_len = len(str(len(all_chats)))
+    res_dict = dict()
+    for i in range(len(all_chats)):
+        real_name = all_chats[i].find(class_="body").find(class_="name").text.strip()
+        res_dict[f"chat_{i + 1:0{max_len}}"] = real_name
+    return res_dict
+
+# for i in res_dict:
+#     print(i, res_dict[i])
+# print(len(all_chats))
+# chats_path = os.path.join("data", "chats")
+# chat_names = [chat_name for chat_name in os.listdir(chats_path)]
+# print(*chat_names, sep="\n")
 
 
-tmp_chats = ["chat_001", "chat_002", "chat_003", "chat_004", "chat_005", "chat_006", "chat_007", "chat_008", "chat_009",
-             "chat_010", "chat_011", "chat_012", "chat_013", "chat_014"]
-tmp_chats = ["chat_008"]
-
-for chat_name in tmp_chats:
-    chat = Chat(chat_name, "null")
-
-for message in chat.messages:
-    print(message.author, message.datetime)
-
-# for i in messages_list:
-#     print(i)
-#     print("--------------------------")
-# for i in range(5):
-#     print(messages_list_part[i].prettify())
-#     print("----------")
-
-
-# with open("out.html", "w", encoding="utf-8") as f_out:
-#     print(messages_list_part.prettify(), file=f_out)
+# # tmp_chats = ["chat_001", "chat_002", "chat_003", "chat_004", "chat_005", "chat_006", "chat_007", "chat_008", "chat_009", "chat_010", "chat_011", "chat_012", "chat_013", "chat_014"]
+# tmp_chats = ["chat_008"]
+#
+# for chat_name in tmp_chats:
+#     chat = Chat(chat_name, "null")
+#
+# for message in chat.messages:
+#     soup = message.soup
+#     media_wrap = soup.find("div", class_="media_wrap")
+#     if media_wrap is None:
+#         pass
+#     else:
+#         media_classes = media_wrap.find("div", class_="media")["class"]
+#         if "media_photo" in media_classes:
+#             pass
+#         elif "media_call" in media_classes:
+#             pass
+#         elif "media_voice_message" in media_classes:
+#             pass
+#         elif "media_video" in media_classes:
+#             pass
+#         elif "media_file" in media_classes:
+#             pass
+#         elif "media_audio_file" in media_classes:
+#             pass
+#         else:
+#             print(message.author, message.datetime)
+#             print(media_classes)
+#             sys.exit(1)
